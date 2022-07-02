ee28caa2024a547ae8aaba01f0cfe4263518e4b1
iubizi <106417012+iubizi@users.noreply.github.com>
2022-07-01 20:50:59 -0400

    Add files via upload

---
+++ slurm/2_38.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J mat_2_38
+#SBATCH -p general
+#SBATCH -o txt/mat_2_38_%j.txt
+#SBATCH -e err/mat_2_38_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jxu4@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+
+#Load any modules that your program needs
+module load matlab
+
+#Run your program
+matlab -r "function_carbonate_pointNet('2/2_38.bin')"
+matlab -r "function_carbonate_pointNet('3/2_38.bin')"
---
+++ slurm/4_10.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J mat_4_10
+#SBATCH -p general
+#SBATCH -o txt/mat_4_10_%j.txt
+#SBATCH -e err/mat_4_10_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jxu4@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+
+#Load any modules that your program needs
+module load matlab
+
+#Run your program
+matlab -r "function_carbonate_pointNet('4/4_10.bin')"
+matlab -r "function_carbonate_pointNet('5/4_10.bin')"
