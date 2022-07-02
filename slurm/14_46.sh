54606c8297affde0c1d6793146c88ba0467f2b76
iubizi <106417012+iubizi@users.noreply.github.com>
2022-07-01 20:54:56 -0400

    Add files via upload

---
+++ slurm/14_46.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J mat_14_46
+#SBATCH -p general
+#SBATCH -o txt/mat_14_46_%j.txt
+#SBATCH -e err/mat_14_46_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jxu4@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+
+#Load any modules that your program needs
+module load matlab
+
+#Run your program
+matlab -r "function_carbonate_pointNet('14/14_46.bin')"
+matlab -r "function_carbonate_pointNet('15/14_46.bin')"
