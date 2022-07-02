ab4896d93c5d76eb24ab100df5a21379da29b4a9
iubizi <106417012+iubizi@users.noreply.github.com>
2022-07-01 20:53:19 -0400

    Add files via upload

---
+++ slurm/9_54.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -J mat_9_54
+#SBATCH -p general
+#SBATCH -o txt/mat_9_54_%j.txt
+#SBATCH -e err/mat_9_54_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jxu4@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+
+#Load any modules that your program needs
+module load matlab
+
+#Run your program
+matlab -r "function_carbonate_pointNet('54/9_54.bin')"
