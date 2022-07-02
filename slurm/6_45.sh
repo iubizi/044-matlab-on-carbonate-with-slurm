24fdf8959591471745d34bf7580efaf2105f31c6
iubizi <106417012+iubizi@users.noreply.github.com>
2022-07-01 20:52:01 -0400

    Add files via upload

---
+++ slurm/6_45.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J mat_6_45
+#SBATCH -p general
+#SBATCH -o txt/mat_6_45_%j.txt
+#SBATCH -e err/mat_6_45_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jxu4@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+
+#Load any modules that your program needs
+module load matlab
+
+#Run your program
+matlab -r "function_carbonate_pointNet('6/6_45.bin')"
+matlab -r "function_carbonate_pointNet('7/6_45.bin')"
